b734a6a1418ffe71f918fe01b6d338072465a23c
lijiacheng <lijiacheng0224@163.com>
2025-07-21 14:16:37 +0800

    mod: 添加登录页面

--- src/views/login/index.tsx
+++ src/views/login/index.tsx
@@ -1,23 +1,61 @@
-import { Form, Button, Lottie } from '@douyinfe/semi-ui'
+import { Form, Button, Lottie, Toast } from '@douyinfe/semi-ui'
 import { IconUserCircle, IconLock } from '@douyinfe/semi-icons'
 import { login } from '@/api/user'
+import { getCaptchaImg } from '@/api/system'
 import type { LoginParams } from '@/api/user'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router'
+import { setToken } from '@/utils/auth'
 
 import animationData from '@/views/login/Frankenstein.json'
 
 const LoginForm = () => {
-  const handleSubmit = (
-    values: LoginParams,
-    event: React.FormEvent<HTMLFormElement> | undefined
-  ) => {
-    console.log('event ==> ', event)
+  const navigate = useNavigate()
+  const [formValue, setFormValue] = useState<LoginParams>({
+    username: '',
+    password: '',
+    code: '',
+    uuid: ''
+  })
+
+  const [captchaImage, setCaptchaImage] = useState('')
+
+  const handleSubmit = (values: LoginParams) => {
     login({
-      ...values
-    }).then((res) => {
-      console.log(res)
+      ...values,
+      uuid: formValue.uuid
     })
+      .then((res) => {
+        const { code, message, result } = res.data
+        if (code == 200) {
+          setToken(result.token)
+          navigate('/')
+        } else {
+          Toast.error(message)
+        }
+      })
+      .catch(() => {
+        getCaptcha()
+      })
   }
+
+  const getCaptcha = async () => {
+    try {
+      const res = await getCaptchaImg()
+      const { result } = res.data
+      setCaptchaImage(`data:image/png;base64,${result.img}`)
+      setFormValue({
+        ...formValue,
+        uuid: result.uuid
+      })
+    } catch {
+      Toast.error('获取验证码失败')
+    }
+  }
+
+  useEffect(() => {
+    getCaptcha()
+  }, [])
 
   return (
     <div className="w-full h-full min-h-screen flex justify-center items-center">
@@ -49,6 +87,24 @@ const LoginForm = () => {
               field="password"
               initValue=""
               mode="password"></Form.Input>
+
+            <div className="flex gap-2 items-center">
+              <Form.Input
+                label="验证码"
+                noLabel
+                placeholder="请输入验证码"
+                field="code"
+                initValue=""></Form.Input>
+
+              <div
+                className="cursor-pointer rounded border"
+                onClick={getCaptcha}>
+                <img
+                  className="h-[30px] w-[100px] object-cover"
+                  src={captchaImage}
+                />
+              </div>
+            </div>
 
             <Button block htmlType="submit">
               Login
