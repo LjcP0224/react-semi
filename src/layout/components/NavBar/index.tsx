421770dd0f373942cdb6133711a604bb9bf7d140
lijiacheng <lijiacheng0224@163.com>
2025-07-21 16:22:49 +0800

    feat(layout): 添加导航栏组件
    - 引入 Semi UI 的 Layout、Nav 和 Avatar 组件
    - 实现了一个基本的导航栏结构，包括标题和用户头像
    - 优化了组件名称，将 Navbar 改为 NavBar

--- src/layout/components/NavBar/index.tsx
+++ src/layout/components/NavBar/index.tsx
@@ -1,9 +1,20 @@
-function Navbar({ show }: { show: boolean }) {
+import { Layout, Nav, Avatar } from '@douyinfe/semi-ui'
+
+function NavBar({ show }: { show: boolean }) {
   if (!show) {
     return <div className=" fixed right-0 bottom-52 z-50"></div>
   }
 
-  return <div>a</div>
+  return (
+    <Layout.Header className="bg-[var(--semi-color-fill-0)] fixed top-0 left-0 right-0 z-50 h-16">
+      <Nav mode="horizontal">
+        <Nav.Header>NYMJ</Nav.Header>
+        <Nav.Footer>
+          <Avatar>HT</Avatar>
+        </Nav.Footer>
+      </Nav>
+    </Layout.Header>
+  )
 }
 
-export default Navbar
+export default NavBar
