765e49359760c1d35c0dadf492e02fcb58d5933c
lijiacheng <lijiacheng0224@163.com>
2025-07-23 16:09:29 +0800

    feat(locale): 优化国际化组件显示逻辑
    - 添加 triggerRender 方法，自定义选择框显示内容
    - 使用 Button 组件包裹语言切换按钮，提升用户体验
    - 引入 isString 函数，增强类型安全性

--- src/locale/locale.tsx
+++ src/locale/locale.tsx
@@ -1,21 +1,32 @@
 import { useContext } from 'react'
-import { Select } from '@douyinfe/semi-ui'
+import { Button, Select } from '@douyinfe/semi-ui'
 import { IconLanguage } from '@douyinfe/semi-icons'
 
 import { LOCALE_OPTIONS } from '@/locale/index'
 import { GlobalContext } from '@/context'
+import { isString } from 'es-toolkit'
 
 const Locale = () => {
   const { setLang, lang } = useContext(GlobalContext)
 
+  const triggerRender = () => {
+    return (
+      <Button icon={<IconLanguage />}>{lang == 'zh-CN' ? '中' : 'En'}</Button>
+    )
+  }
+
   const handleChange = (value: unknown) => {
-    if (setLang) {
-      setLang(value as string)
+    if (setLang && isString(value)) {
+      setLang(value)
     }
   }
 
   return (
-    <Select value={lang} onChange={handleChange} prefix={<IconLanguage />} >
+    <Select
+      value={lang}
+      onChange={handleChange}
+      prefix={<IconLanguage />}
+      triggerRender={triggerRender}>
       {LOCALE_OPTIONS.map((item) => (
         <Select.Option value={item.value} key={item.value}>
           {item.label}
