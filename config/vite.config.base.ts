6c96c0f217b33af2b99540de87498262b3b2d0d0
ljc <lijiacheng0224@163.com>
2025-07-30 14:19:44 +0800

    build(deps): 移除 tanstack-router 插件

--- config/vite.config.base.ts
+++ config/vite.config.base.ts
@@ -1,20 +1,12 @@
 import { defineConfig } from "vite";
 import type { UserConfig } from "vite";
-import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tanstackRouter({
-      target: "react",
-      autoCodeSplitting: true,
-    }),
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: [
       {
